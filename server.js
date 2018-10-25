c96a9a230b5358e7242ac75ce2809b84288cde35
Joshua Ho <ho.joshua4@gmail.com>
2018-10-24 19:43:35 -0700

    IT'S ALIVE

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/express_backend', (req, res) => {
+  res.send( {
+    express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'
+  });
+});
